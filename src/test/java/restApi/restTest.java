8e23993e814e52f0b24e8d4f282a9ee744e921fd
George Helidze <>
2018-06-05 17:24:17 +0300

    Some tests

--- src/test/java/restApi/restTest.java
+++ src/test/java/restApi/restTest.java
@@ -2,6 +2,8 @@ package restApi;
 
 import org.testng.annotations.Test;
 
+import java.util.Date;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
 
@@ -42,7 +44,7 @@ public class restTest {
     }
 
     @Test
-    public void med4Auth(){
+    public void testUserProfileInfo(){
         given().
                 contentType("application/json").
                 header("Authorization", "Bearer "+token).
@@ -51,7 +53,81 @@ public class restTest {
                 then().
                 assertThat().
                 statusCode(200).
+                body(containsString("user")).
+                body(containsString("billingProfile")).
+                body(containsString("shipProfile")).
                 log().all();
     }
+
+    @Test
+    public void loginWithIncorrectEmail(){
+        given().
+                header("Content-Type", "application/json").
+                header("Cache-Control", "no-cache").
+                body("{\n  \"email\": \"user@ad.min1\",\n  \"password\": \"1q2w3e4r\"\n}").
+                post(URI + "/Auth/Login").
+                then().
+                assertThat().
+                statusCode(406).
+                body(containsString("{\"code\":\"NotFound\",\"message\":\"Get User Error. User not exist with email user@ad.min1\"}")).
+                log().body();
+    }
+
+    @Test
+    public void loginWithIncorrectPassword(){
+        given().
+                header("Content-Type", "application/json").
+                header("Cache-Control", "no-cache").
+                body("{\n  \"email\": \"user@ad.min\",\n  \"password\": \"1q2w3e4r1\"\n}").
+                post(URI + "/Auth/Login").
+                then().
+                assertThat().
+                statusCode(406).
+                body(containsString("{\"code\":\"Invalid\",\"message\":\"Password is invalid\"}")).
+                log().body();
+    }
+
+    @Test
+    public void newUserRegistrationWithExistEmail(){
+        given().
+                header("Content-Type", "application/json").
+                header("Cache-Control", "no-cache").
+                body("{\n" +
+                        "  \"email\": \"string@string.com\",\n" +
+                        "  \"password\": \"string\",\n" +
+                        "  \"firstName\": \"string\",\n" +
+                        "  \"lastName\": \"string\",\n" +
+                        "  \"isSubscribe\": true\n" +
+                        "}").
+                post(URI + "/Auth/Registration").
+                then().
+                assertThat().
+                statusCode(406).
+                body(containsString("{\"code\":\"Invalid\",\"message\":\"Email already exist\"}")).
+                log().body();
+    }
+
+//    @Test
+//    public void newUserRegistration(){
+//        Date date = new Date();
+//        given().
+//                header("Content-Type", "application/json").
+//                header("Cache-Control", "no-cache").
+//                body("{\n" +
+//                        "  \"email\": \"string@string.com\",\n" +
+//                        "  \"password\": \"string\",\n" +
+//                        "  \"firstName\": \"string\",\n" +
+//                        "  \"lastName\": \"string\",\n" +
+//                        "  \"isSubscribe\": true\n" +
+//                        "}").
+//                post(URI + "/Auth/Registration").
+//                then().
+//                assertThat().
+//                statusCode(406).
+//                body(containsString("{\"code\":\"Invalid\",\"message\":\"Email already exist\"}")).
+//                log().body();
+//    }
+
+
 }
 
